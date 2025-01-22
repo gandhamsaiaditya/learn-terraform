aa9b458a536bc8e71ca1de8daca6641ca9777f35
saiad <saiaditya.gandham@gmail.com>
2025-01-22 15:55:07 +0530

    dfgg

--- Roboshop-v1/data.tf
+++ Roboshop-v1/data.tf
@@ -6,5 +6,5 @@ data "azurerm_subnet" "internal" {
   name                 = "internal"
   resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = "Vintech-network"
-  address_prefixes     = ["10.0.2.0/24"]
+
 }
