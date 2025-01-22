3cbf73303a74737edd879a0d271e39ea11271951
saiad <saiaditya.gandham@gmail.com>
2025-01-22 13:30:37 +0530

    dfg

--- 03-Resource/data.tf
+++ 03-Resource/data.tf
@@ -2,8 +2,4 @@
   name = "Project-Setup"
 }
 
-data "azurerm_subnet" "example" {
-  name                 = "default"
-  virtual_network_name = "workshop-vnet"
-  resource_group_name  = "data.azurerm_resource_group.example.name"
-}
+
