0abd4ed6ebf752fc8cbbeeb946701b31c5ebdd07
saiad <saiaditya.gandham@gmail.com>
2025-01-22 13:22:34 +0530

    dfg

--- 03-Resource/provider.tf
+++ 03-Resource/provider.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
-  subscription_id = "a92e07d8-3cdd-4fda-bb98-99b2dddb739c"
+  subscription_id = "560c39c6-a3a6-4e68-aa91-6418a1940cbd"
 
   features {}
 }
---
+++ Roboshop-v1/provider.tf
@@ -0,0 +1,5 @@
+provider "azurerm" {
+  subscription_id = "560c39c6-a3a6-4e68-aa91-6418a1940cbd"
+
+  features {}
+}
