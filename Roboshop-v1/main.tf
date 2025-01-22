4eb1fcf51131b43fe2387e8b8f9270e8cf5fbfd6
saiad <saiaditya.gandham@gmail.com>
2025-01-22 21:29:03 +0530

    dfgg

--- Roboshop-v1/main.tf
+++ Roboshop-v1/main.tf
@@ -1,7 +1,7 @@
 module "components"
 {
-  for_each = var.components
-  source = "./vm"
+  for_each  = var.components
+  source    = "./vm"
   component = each.value["name"]
   vm_size   = each.value["vm_size"]
 }
