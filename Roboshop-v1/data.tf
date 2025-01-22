4ea61c56e550d1b63918ac337eb6530916aa0a5a
saiad <saiaditya.gandham@gmail.com>
2025-01-22 15:53:09 +0530

    dfgg

---
+++ Roboshop-v1/data.tf
@@ -0,0 +1,11 @@
+data "azurerm_resource_group" "rg" {
+  name     = "Vintech"
+  location = "UK West"
+}
+
+data "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = data.azurerm_resource_group.rg.name
+  virtual_network_name = "Vintech-network"
+  address_prefixes     = ["10.0.2.0/24"]
+}
--- Roboshop-v1/main.tf
+++ Roboshop-v1/main.tf
@@ -1,38 +1,22 @@
-resource "azurerm_resource_group" "rg" {
-  name     = "Vintech"
-  location = "UK West"
-}
 
-resource "azurerm_virtual_network" "vn" {
-  name                = "Vintech-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
 
-resource "azurerm_subnet" "internal" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vn.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
 
 resource "azurerm_network_interface" "ni" {
   name                = "Vintech-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "testconfiguration1"
-    subnet_id                     = azurerm_subnet.internal.id
+    subnet_id                     =data.azurerm_subnet.internal.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
 resource "azurerm_virtual_machine" "main" {
   name                  = "Frontend-vm"
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
+  location              = data.azurerm_resource_group.rg.location
+  resource_group_name   = data.azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.ni.id]
   vm_size               = "Standard_DS1_v2"
 
