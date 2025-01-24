14cba4e0688ac027a5731809f908ec37050a8a17
saiad <saiaditya.gandham@gmail.com>
2025-01-24 22:50:47 +0530

    dfgg

--- Roboshop-v1/vm/main.tf
+++ Roboshop-v1/vm/main.tf
@@ -62,11 +62,11 @@ resource "azurerm_dns_a_record" "main" {
 
 
 resource "azurerm_virtual_machine" "main" {
-  name                  = var.component
-  location              = data.azurerm_resource_group.main.location
-  resource_group_name   = data.azurerm_resource_group.main.name
+  name                = var.component
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
   network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = var.vm_size
+  vm_size = var.vm_size
 
   # Uncomment this line to delete the OS disk automatically when deleting the VM
   delete_os_disk_on_termination = true
@@ -75,7 +75,7 @@ resource "azurerm_virtual_machine" "main" {
   # delete_data_disks_on_termination = true
 
   storage_image_reference {
-  id = "/subscriptions/560c39c6-a3a6-4e68-aa91-6418a1940cbd/resourceGroups/project-setup-1/providers/Microsoft.Compute/galleries/CreatedImage/images/VM_definition/versions/1.0.0"
+    id = "/subscriptions/560c39c6-a3a6-4e68-aa91-6418a1940cbd/resourceGroups/project-setup-1/providers/Microsoft.Compute/galleries/CreatedImage/images/VM_definition/versions/1.0.0"
   }
   storage_os_disk {
     name              = var.component
@@ -91,23 +91,26 @@ resource "azurerm_virtual_machine" "main" {
   os_profile_linux_config {
     disable_password_authentication = false
   }
- # provisioner "remote-exec" {
-
-  #  connection {
-   #   type     = "ssh"
-  #    user     = "aditya"
-   #   password = "aditya@123456"
-  #    host     = azurerm_public_ip.main.ip_address
-  #  }
-  #  inline = [
-
-   #   "sudo dnf install python3.12-pip -y",
-   #   "sudo pip3.12 install ansible",
-   #   "ansible-pull -i localhost, -u https://github.com/gandhamsaiaditya/Roboshop-shell -e app.name=${var.component} -e env = dev"
-
-  #  ]
- # }
 }
+resource "null_resource" "ansible" {
+    provisioner "remote-exec" {
+
+      connection {
+        type     = "ssh"
+        user     = "aditya"
+        password = "aditya@123456"
+        host     = azurerm_public_ip.main.ip_address
+      }
+      inline = [
+
+        "sudo dnf install python3.12-pip -y",
+        "sudo pip3.12 install ansible",
+        "ansible-pull -i localhost, -u https://github.com/gandhamsaiaditya/Roboshop-shell -e app.name=${var.component} -e env = dev"
+
+      ]
+    }
+  }
+
 
 
 
